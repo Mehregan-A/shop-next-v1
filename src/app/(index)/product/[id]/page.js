8b01f74bd994c36dc294c2fcc2aacb69e13dd43a
Mehregan <Mehreganarshiya@gmail.com>
2026-02-24 23:13:31 +0330

    practice server rendering

--- src/app/(index)/product/[id]/page.js
+++ src/app/(index)/product/[id]/page.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const getData=async (input) => {
+    const data = await fetch(`https://api.vercel.app/blog/${input}`,{cache:"force-cache"})
+    return await data.json();
+}
+
 const Page = async ({params}) => {
-    const {id} = await params;
+    const {id} =await params;
+    const result = await getData(id);
+    console.log(result);
     return (
         <div>
-            {id}
+            {result.title}
+            {result.category}
         </div>
     );
 };
--- src/app/(index)/product/page.js
+++ src/app/(index)/product/page.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Page = () => {
+const getData=async () => {
+    const data = await fetch('https://api.vercel.app/blog')
+    return await data.json();
+}
+
+const Page = async () => {
+    const result = await getData();
+    console.log(result);
     return (
         <div>
-            
+            {result && result.length>0 && result?.map((item, index) => (
+                <div key={index}>{item.title}</div>
+            ))}
         </div>
     );
 };
